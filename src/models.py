ddddd03c4076b6f3611e0ed7597ef3b0f9464fb9
Alcides Rivera <elarsi626@gmail.com>
2026-01-31 19:35:29 +0000

    First

--- src/models.py
+++ src/models.py
@@ -8,12 +8,29 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            
+        }
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120),nullable=False)
+    quote: Mapped[str] = mapped_column(String(120),nullable=False)
+    image: Mapped[str] = mapped_column(String(120),nullable=False)
+    job: Mapped[str] = mapped_column(String(120),nullable=False)
+    age: Mapped[str] = mapped_column(String(120),nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "quote": self.quote,
+            "image": self.image,
+            "job": self.job,
+            "age": self.age
         }
